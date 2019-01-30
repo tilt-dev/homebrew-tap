e25313f4387fa06c60e58923e24e3cc2edc43ee7
Windmill Engineering <hi@windmill.engineering>
2019-01-30 09:48:24 -0500

    Brew formula update for tilt version v0.5.1

--- tilt.rb
+++ tilt.rb
@@ -1,9 +1,9 @@
 class Tilt < Formula
   desc "Local Kubernetes development with no stress"
   homepage "https://tilt.build/"
-  url "http://github.com/windmilleng/tilt/releases/v0.5.1/tilt.0.5.1.mac.x86_64.tar.gz"
+  url "http://github.com/windmilleng/tilt/releases/download/v0.5.1/tilt.0.5.1.mac.x86_64.tar.gz"
   version "0.5.1"
-  sha256 "6b0cdcd5a4154a6292b8b331cdf324fcbca2572bc5e8f20e42721646c73dcf99"
+  sha256 "a56da034d6ab6303041c014078eeb22212122a43c773d45d545195e9b8cd9ad2"
   
   depends_on "docker"
   depends_on "kubernetes-cli"
