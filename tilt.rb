71898c1a242077fac3083777d960977721676d3a
Windmill Engineering <hi@windmill.engineering>
2019-08-13 17:09:38 -0400

    Brew formula update for tilt version v0.10.3

--- tilt.rb
+++ tilt.rb
@@ -2,9 +2,9 @@
 class Tilt < Formula
   desc "Local Kubernetes development with no stress"
   homepage "https://tilt.build/"
-  url "http://github.com/windmilleng/tilt/releases/download/v0.10.2/tilt.0.10.2.mac.x86_64.tar.gz"
-  version "0.10.2"
-  sha256 "17b319371030fd4131b061800c49f7bd58dfe272d82c5dbbfa2d12c7c92dcfc8"
+  url "http://github.com/windmilleng/tilt/releases/download/v0.10.3/tilt.0.10.3.mac.x86_64.tar.gz"
+  version "0.10.3"
+  sha256 "e6a33a54e0eceaa90fa7a8c29d62035a183b516e2f2507d981e64b261d00f0ea"
 
   def install
     bin.install "tilt"
