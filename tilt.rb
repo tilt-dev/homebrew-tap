53e8e96141ed6236d149a27a2aa5d0cb32c99a11
Windmill Engineering <hi@windmill.engineering>
2020-04-01 11:56:07 -0400

    Brew formula update for tilt version v0.12.11

--- tilt.rb
+++ tilt.rb
@@ -2,16 +2,16 @@
 class Tilt < Formula
   desc "Local Kubernetes development with no stress"
   homepage "https://tilt.build/"
-  version "0.12.10"
+  version "0.12.11"
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/windmilleng/tilt/releases/download/v0.12.10/tilt.0.12.10.mac.x86_64.tar.gz"
-    sha256 "18e730c60c96a390dc09effde28e1ca51caab7f3cc4d040ff9492326faf4dbf1"
+    url "http://github.com/windmilleng/tilt/releases/download/v0.12.11/tilt.0.12.11.mac.x86_64.tar.gz"
+    sha256 "c12eea8658188cfdb8f434d59030d736efb72b1c5e7f0b5c58b8f8ac60f783e4"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "http://github.com/windmilleng/tilt/releases/download/v0.12.10/tilt.0.12.10.linux.x86_64.tar.gz"
-      sha256 "808feddce591833610747534fcce551dbfc6c7e352fb7492d9437e5b213cda24"
+      url "http://github.com/windmilleng/tilt/releases/download/v0.12.11/tilt.0.12.11.linux.x86_64.tar.gz"
+      sha256 "78360ac511676b73a9ea95f153323aa7ada0b272849699e15cb7f351db679ec5"
     end
   end
 
