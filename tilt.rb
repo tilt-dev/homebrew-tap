808e0a37c71aceeedec8cc307e7827e9266e4c62
Tilt Dev <hi@tilt.dev>
2020-08-25 17:56:12 -0400

    Brew formula update for tilt version v0.17.3

--- tilt.rb
+++ tilt.rb
@@ -2,24 +2,24 @@
 class Tilt < Formula
   desc "Tilt powers multi-service developments for teams that deploy to Kubernetes."
   homepage "https://tilt.dev/"
-  version "0.17.2"
+  version "0.17.3"
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/tilt-dev/tilt/releases/download/v0.17.2/tilt.0.17.2.mac.x86_64.tar.gz"
-    sha256 "b9d42b80bec790176368d22a79fe18843d06c91951ff7cb5e9955ee7057de40d"
+    url "http://github.com/tilt-dev/tilt/releases/download/v0.17.3/tilt.0.17.3.mac.x86_64.tar.gz"
+    sha256 "2a1e8118a5e70319f55a1e2fd26b42b52c1010c1e57dda8bc2572e2963d3b934"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "http://github.com/tilt-dev/tilt/releases/download/v0.17.2/tilt.0.17.2.linux.x86_64.tar.gz"
-      sha256 "8bd5e67c54fff0073444f7875ba44f120ce174b54325509312050c52ee42e36a"
+      url "http://github.com/tilt-dev/tilt/releases/download/v0.17.3/tilt.0.17.3.linux.x86_64.tar.gz"
+      sha256 "b0ee99903c12b87b2e3c635f4501f60d58dd3e9fa4d0ff1b11ae8b3213d97ecd"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "http://github.com/tilt-dev/tilt/releases/download/v0.17.2/tilt.0.17.2.linux.arm64_ALPHA.tar.gz"
-        sha256 "f82c2932e5b1c4e8a7bcf23e8dacc4f700c8d946789d64385531ab0fa63a05ef"
+        url "http://github.com/tilt-dev/tilt/releases/download/v0.17.3/tilt.0.17.3.linux.arm64_ALPHA.tar.gz"
+        sha256 "dcfa4b19c2189024ff59682eb041ccb25768b8508bb5fa85884ac32f101470ab"
       else
-        url "http://github.com/tilt-dev/tilt/releases/download/v0.17.2/tilt.0.17.2.linux.arm_ALPHA.tar.gz"
-        sha256 "d4031175005c92192ba5c21306bd55b62587c5716246e8bd1edb1c8fe14881d6"
+        url "http://github.com/tilt-dev/tilt/releases/download/v0.17.3/tilt.0.17.3.linux.arm_ALPHA.tar.gz"
+        sha256 "a41fe4ecc3b2aa2c6469da6745eae31d4a9361fd8659ca260c4fbfc4e3e004c4"
       end
     end
   end
