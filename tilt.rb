63ee9e939b877958b679de4743e43d7826041ab6
Tilt Dev <hi@tilt.dev>
2020-12-01 20:24:10 -0500

    Brew formula update for tilt version v0.17.13

--- tilt.rb
+++ tilt.rb
@@ -2,24 +2,24 @@
 class Tilt < Formula
   desc "Tilt powers multi-service developments for teams that deploy to Kubernetes."
   homepage "https://tilt.dev/"
-  version "0.17.12"
+  version "0.17.13"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.17.12/tilt.0.17.12.mac.x86_64.tar.gz"
-    sha256 "a87b11342182bbd13e559423aa455ad54c8a1865682eee6cb6e57571fe77e677"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.17.13/tilt.0.17.13.mac.x86_64.tar.gz"
+    sha256 "40fa8ce5381f75082ed3734be7c1ab5f4d5eef935ec7e7cc78e396db005b2c2f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tilt-dev/tilt/releases/download/v0.17.12/tilt.0.17.12.linux.x86_64.tar.gz"
-      sha256 "2e8f4fdcdb94c987ec0757e093698a933c22045c5567b7b354c6658605cc5cb0"
+      url "https://github.com/tilt-dev/tilt/releases/download/v0.17.13/tilt.0.17.13.linux.x86_64.tar.gz"
+      sha256 "a0614286a804e3b68fb80d13cce1ccfd0b28bb5e19d6108cc3c69a653a751ecd"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/tilt-dev/tilt/releases/download/v0.17.12/tilt.0.17.12.linux.arm64_ALPHA.tar.gz"
-        sha256 "dbc8edbebfdd20877d608a0898267f304737fb9d389eb9b06ce16b9580e08334"
+        url "https://github.com/tilt-dev/tilt/releases/download/v0.17.13/tilt.0.17.13.linux.arm64_ALPHA.tar.gz"
+        sha256 "59f1f96ee181964a14df5c2046531a8876bc0761fc8ee7d6943864f8092c2632"
       else
-        url "https://github.com/tilt-dev/tilt/releases/download/v0.17.12/tilt.0.17.12.linux.arm_ALPHA.tar.gz"
-        sha256 "f6c10c278e33306577b6d92e255447c460eac596c092cb6c73762ff87c61cc09"
+        url "https://github.com/tilt-dev/tilt/releases/download/v0.17.13/tilt.0.17.13.linux.arm_ALPHA.tar.gz"
+        sha256 "63d0a5daf55736c9096db8d662dd627e21679cdca4abe737e203ef5e3f33105e"
       end
     end
   end
