c80a2d88993e7fd21324def71a75f99fe88747e4
Tilt Dev <hi@tilt.dev>
2021-01-05 12:57:24 -0500

    Brew formula update for tilt version v0.18.3

--- tilt.rb
+++ tilt.rb
@@ -2,24 +2,24 @@
 class Tilt < Formula
   desc "Tilt powers multi-service developments for teams that deploy to Kubernetes."
   homepage "https://tilt.dev/"
-  version "0.18.2"
+  version "0.18.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.18.2/tilt.0.18.2.mac.x86_64.tar.gz"
-    sha256 "fdee0bf48b07b448ea90e557f71901a8825d2050174b8d79cb97c4f9ac0b990f"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.18.3/tilt.0.18.3.mac.x86_64.tar.gz"
+    sha256 "4da09e32ec3f9a2ebbb532d99bcb888ffc55c0f73b2e2b41298db8529c8b04c7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tilt-dev/tilt/releases/download/v0.18.2/tilt.0.18.2.linux.x86_64.tar.gz"
-      sha256 "4a6cae26fc78ee8640a416bf53f95cc9ae214543ad5b20ac402654caef4967cb"
+      url "https://github.com/tilt-dev/tilt/releases/download/v0.18.3/tilt.0.18.3.linux.x86_64.tar.gz"
+      sha256 "3584a0a2c1a12802f00534b59e655d7b3a1be0f625132b6351cf6f36a1e95810"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/tilt-dev/tilt/releases/download/v0.18.2/tilt.0.18.2.linux.arm64_ALPHA.tar.gz"
-        sha256 "d5115731f0d3cd06c911deb9c887fc2f3f9b01fc46286f0fd5cecc5477bc6303"
+        url "https://github.com/tilt-dev/tilt/releases/download/v0.18.3/tilt.0.18.3.linux.arm64_ALPHA.tar.gz"
+        sha256 "27485c8ba08981fd0ebbc0e09347f66382efbde73250c3a0c6b84cf26b976b31"
       else
-        url "https://github.com/tilt-dev/tilt/releases/download/v0.18.2/tilt.0.18.2.linux.arm_ALPHA.tar.gz"
-        sha256 "ac465fe942c087808c5f65bf523e2ea1394d885a9388cd6f7e09b96a705a90f4"
+        url "https://github.com/tilt-dev/tilt/releases/download/v0.18.3/tilt.0.18.3.linux.arm_ALPHA.tar.gz"
+        sha256 "7aebf0f318adefdbdaf5591a184a2761636a380d5bba33b8535d17ab75fda789"
       end
     end
   end
