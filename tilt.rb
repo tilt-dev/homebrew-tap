a597a6c15fc6376c39c6afb8737c7ae564a0b625
Tilt Dev <hi@tilt.dev>
2021-04-23 16:01:29 -0400

    Brew formula update for tilt version v0.19.7

--- tilt.rb
+++ tilt.rb
@@ -5,28 +5,28 @@
 class Tilt < Formula
   desc "Tilt powers multi-service developments for teams that deploy to Kubernetes."
   homepage "https://tilt.dev/"
-  version "0.19.6"
+  version "0.19.7"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.6/tilt.0.19.6.mac.x86_64.tar.gz"
-    sha256 "adf7d2bdaad743c87ef7f266a5c20413c49d80d438ba709cc27823d42e305b59"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.7/tilt.0.19.7.mac.x86_64.tar.gz"
+    sha256 "9f68cbae7d99629ac868716e7ab5c8ffce9bc4cc0d5ec94c52e55c9a653d942e"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.6/tilt.0.19.6.mac.arm64_ALPHA.tar.gz"
-    sha256 "0ecf19b079649027f76e631b40eef020fe1237c95ae4c7eb5073e48651a26630"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.7/tilt.0.19.7.mac.arm64_ALPHA.tar.gz"
+    sha256 "d6dbdb176c660c2c0202cb819ae7f4fa300395c37a59695c3bc5a56de2f0ca16"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.6/tilt.0.19.6.linux.x86_64.tar.gz"
-    sha256 "bd53fd580a7d48af0c4b8c334c9b1b36c00580844cd5838efe6df5707557f0db"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.7/tilt.0.19.7.linux.x86_64.tar.gz"
+    sha256 "ec1f72a55a54003a81e57201f8f62154e49073aed3a6e5e45521918c4a8c83d4"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.6/tilt.0.19.6.linux.arm_ALPHA.tar.gz"
-    sha256 "0752c298ec4e2637c308ac98bb369bab58323f621e861f9491adc6908676b92b"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.7/tilt.0.19.7.linux.arm_ALPHA.tar.gz"
+    sha256 "aa83cd8097d3c238ade0ea2625ddc4664238cf7812e491ebc68b243abc5a53c5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.6/tilt.0.19.6.linux.arm64_ALPHA.tar.gz"
-    sha256 "04f7f833c0128ddc6531230befee366ea46b6ef83d713f5d4c7ec7f1973a9f02"
+    url "https://github.com/tilt-dev/tilt/releases/download/v0.19.7/tilt.0.19.7.linux.arm64_ALPHA.tar.gz"
+    sha256 "949dd06c66dba8a7e517d0fe549511666c407b3969eb9c7b766b9c1007784d10"
   end
 
   def install
